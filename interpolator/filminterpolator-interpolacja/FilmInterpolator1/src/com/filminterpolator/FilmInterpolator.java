c130f9a39e1758b6b6699d4d5858ba9e10a95550
specu1997 <specu1997@gmail.com>
2018-05-09 15:22:12 +0200

    Zapisywanie i wczytywanie klatek, wstepna petla algorytmu

--- interpolator/filminterpolator-interpolacja/FilmInterpolator1/src/com/filminterpolator/FilmInterpolator.java
+++ interpolator/filminterpolator-interpolacja/FilmInterpolator1/src/com/filminterpolator/FilmInterpolator.java
@@ -6,7 +6,6 @@ package com.filminterpolator;
 public class FilmInterpolator {
 
     public static void main(String[] args) {
-        //ImageProcessor.processImage(1, "none", null, null);
         GUI InitialGUI = new GUI();
         InitialGUI.openInterfaceWindow();
 
--- interpolator/filminterpolator-interpolacja/FilmInterpolator1/src/com/filminterpolator/IOManager.java
+++ interpolator/filminterpolator-interpolacja/FilmInterpolator1/src/com/filminterpolator/IOManager.java
@@ -1,20 +1,30 @@
 package com.filminterpolator;
 
 import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.File;
 
 public abstract class IOManager {
 
-    boolean loadFrame(){
-        return true;
-    }
-    static boolean saveFrame(Frame frame){
-        File outputfile = new File(Options.getSingleton().getOutputDirectoryPath());
+    public static Frame loadFrame(int index){
+        try {
+            File inputfile = new File(Options.getSingleton().getInputDirectoryPath() + "//" + Options.getSingleton().getFilename() + String.valueOf(index));
+            BufferedImage image = ImageIO.read(inputfile);
 
+            return new Frame(index, Options.getSingleton().getFilename(), image);
+        }
+        catch (Exception e){
+            System.out.print(e.getMessage());
+            return null;
+        }
+    }
+    public static boolean saveFrame(Frame frame){
+        File outputfile = new File(Options.getSingleton().getOutputDirectoryPath() + "//" + frame.getName());
         try {
             ImageIO.write(frame.getImage(),"png", outputfile);
         } catch (Exception e){
             System.out.print(e.getMessage());
+            return false;
         }
         return true;
     }
